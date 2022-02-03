92b7b96c77d079c38457bc731236d304899d61f3
NadiaFGit <98849881+NadiaFGit@users.noreply.github.com>
2022-02-03 18:35:38 -0500

    Add files via upload

---
+++ storage/framework/views/53abd702a9ad69350624708f2e9a48631dceaede.php
@@ -0,0 +1,43 @@
+
+
+<?php $__env->startSection('content'); ?>
+
+    <h1>Invitation envoyé par <?php echo e($organisateur); ?> </h1><br>
+    <form action="<?php echo e(url('/deconnexion')); ?>" method="Get">
+        <?php echo e(csrf_field()); ?>
+
+        <button class="btn btn-dark" style="float: right" type="submit"> Déconnection</button>
+    </form>
+    <form action="<?php echo e(url('/deconnexion')); ?>" method="Get">
+        <?php echo e(csrf_field()); ?>
+
+        <button class="btn btn-dark" style="float:right;margin-right:20px;" type="submit"> Retour</button>
+    </form>
+
+    <h3>Détail Cadeau: </h3>
+
+    <h5> Titre Cadeau: <?php echo e($detailGift->Title); ?></h5>
+
+    <h5>Prix: <?php echo e($detailGift->Price); ?></h5>
+    <div>
+        <img src="<?php echo e($detailGift->PhotoURL); ?>" style="width:340px;height:200px;" />
+    </div>
+
+    <div>
+        <p>Merci de saisir le montant de votre contribution</p>
+        <form action="<?php echo e(route('participationAccepte')); ?>" method="POST">
+            <?php echo e(csrf_field()); ?>
+
+            <input type="hidden" name="organisateur" value=<?php echo e($organisateur); ?>>
+            <input type="hidden" name="idUserG" value=<?php echo e($idUserG); ?>>
+            <input type="hidden" name="idEvent" value=<?php echo e($idEvent); ?>>
+            <label for="contribution">Montant: </label>
+            <input type="number" name="contribution" value='0.00' min="1"><br><br>
+
+            <button class="btn btn-dark" type="submit" value="valider">Valider</button>
+        </form>
+    </div>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.principal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\User\Desktop\TeamGiftVFinale\TeamGift6\resources\views/detailEvenement.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/558d0e28080e85cc6a73feeab599ea9fa6479b04.php
@@ -0,0 +1,44 @@
+
+<?php $__env->startSection('content'); ?>
+
+    <h1>Mes participations acceptés</h1>
+    <form action="<?php echo e(url('/deconnexion')); ?>" method="Get">
+        <?php echo e(csrf_field()); ?>
+
+        <button class="btn btn-outline-danger" style="float: right" type ="submit"> Déconnection</button>
+    </form>
+    <form action="<?php echo e(route('listeEventsRecus', Session::get('user1'))); ?> " method="Get">
+        <?php echo e(csrf_field()); ?>
+
+        <button class="btn btn-outline-danger" style="float: right" type ="submit">Évenements Reçus</button>
+      </form>
+    <form action="<?php echo e(route('listeMesevenementorganise', Session::get('user'))); ?> " method="Get">
+        <?php echo e(csrf_field()); ?>
+
+        <button class="btn btn-outline-danger" style="float: right" type ="submit">Mes Évenements</button>
+    </form>
+
+    <table class="table table is-bordered">
+        <thead>
+            <tr>
+                <th>Titre événement</th>
+                <th>Date événement</th>
+                <th>État</th>
+                <th>Contribution</th>
+            </tr>
+        </thead>
+
+        <?php $__currentLoopData = $participationsAcceptes; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $participation): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <tr>
+                <td><?php echo e($titre = app\models\Event::find($participation->EventID)->Title); ?></td>
+                <td><?php echo e($titre = app\models\Event::find($participation->EventID)->DateOfEvent); ?></td>
+                <td> accepté </td>
+                <td><?php echo e($participation->Contribution); ?> $ </td>
+            </tr>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+
+    </table>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.principal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\User\Desktop\TeamGiften couleur\TeamGift6\resources\views/listeparticipationAccepter.blade.php ENDPATH**/ ?>
